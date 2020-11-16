1c1c9d06dad629be2dc8a015f0c201e187834075
Leo Giovanetti <hello@leog.me>
2020-11-15 22:51:18 -0300

    EmailSubscriber changes wip

--- EventListener/EmailSubscriber.php
+++ EventListener/EmailSubscriber.php
@@ -86,6 +86,49 @@ class EmailSubscriber extends CommonSubscriber
         $tokens = [];
         $lead = $event->getLead();
         $content = "THE_URL";
+        $config = $this->get('mautic.helper.core_parameters');
+        $auth0ManagementApiToken = $config->getParameter('auth0_management_api_token');
+        $auth0DomainUrl = $config->getParameter('auth0_domain_url');
+
+        $response        = new JsonResponse();
+        $responseContent = [
+            'ticket'       => ''
+        ];
+
+        $client = HttpClient::create([
+            'headers' => [
+                'Authorization' => 'Bearer '.$auth0ManagementApiToken,
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+
+        $parameters    = [
+            "result_url" => "http://myapp.com/callback",
+            "user_id" => "",
+            "connection_id" => "con_0000000000000001",
+            "email" => "",
+            "ttl_sec" => 0,
+            "mark_email_as_verified" => false,
+            "includeEmailInRedirect" => false,
+            'oauth2_access_token' => $accessToken['access_token'],
+            'format'              => 'json',
+        ];
+
+        $url = "https://".$auth0DomainUrl."/api/v2/tickets/password-change";
+
+        try {
+            /** @var ResponseInterface $httpResponse */
+            $httpResponse = $client->request(Request::METHOD_POST, $url);
+        } catch (\Exception $e) {
+            $this->logger->debug("Exception Auth0 EmailSubscriber: ".$e->getMessage());
+            $this->addFlash('plugin.auth0.config.auth0_management_api_token.error', [], 'error');
+        } 
+
+        //$this->addFlash('mautic.config.config.error.not.updated', ['%exception%' => $exception->getMessage()], 'error');
+        //$this->logger->debug("Mark email '".$bouncedRecipient['emailAddress']."' as bounced, reason: ".$bounceCode);
+
+        $data = $this->makeRequest($url, $parameters, 'POST', ['auth_type' => 'rest']);
+
         $tokens['{password_reset_ticket_url}'] = $content;
         $event->addTokens($tokens);
     }
