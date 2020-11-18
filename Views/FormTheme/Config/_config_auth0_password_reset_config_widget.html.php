506eabad10d732f1f6e9434fc4748ddd705f50ad
Leo Giovanetti <hello@leog.me>
2020-11-18 00:10:12 -0300

    Fixing panel title

--- Views/FormTheme/Config/_config_auth0_password_reset_config_widget.html.php
+++ Views/FormTheme/Config/_config_auth0_password_reset_config_widget.html.php
@@ -4,7 +4,7 @@
 
 <div class="panel panel-warning">
     <div class="panel-heading">
-        <h3 class="panel-title"><?php echo $view['translator']->trans('plugin.auth0_password_reset.config.tab.auth0_config'); ?></h3>
+        <h3 class="panel-title"><?php echo $view['translator']->trans('mautic.config.tab.auth0_password_reset_config'); ?></h3>
     </div>
     <div class="panel-body">
         <?php foreach ($form->children as $f): ?>
